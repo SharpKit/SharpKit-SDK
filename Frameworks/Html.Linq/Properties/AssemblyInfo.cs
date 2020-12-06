ee5f74121b8eb5e9497f2c1f5d9ff15896d179b2
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-12-06 19:47:00 +0100

    ported frameworks

--- Frameworks/Html.Linq/Properties/AssemblyInfo.cs
+++ Frameworks/Html.Linq/Properties/AssemblyInfo.cs
@@ -3,36 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using SharpKit.JavaScript;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SharpKit.Html.Linq")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpKit.Html.Linq")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77f1e026-1d54-4da5-8578-29f7c9ea7d74")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.30.2000")]
 [assembly: JsMergedFile(Filename="res/SharpKit.Html.Linq.min.js", Sources=new string[]{"res/SharpKit.Html.Linq.js"}, Minify=true)]
