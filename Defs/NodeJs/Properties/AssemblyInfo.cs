f06b17af937ee0e80825a680d71c3ac91a23cc0f
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-12-06 18:51:19 +0100

    ported defs

--- Defs/NodeJs/Properties/AssemblyInfo.cs
+++ Defs/NodeJs/Properties/AssemblyInfo.cs
@@ -3,37 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using SharpKit.JavaScript;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SharpKit.NodeJs")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpKit.NodeJs")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e3e7127-3d27-4d09-8973-a96da3fa0cbd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.30.2000")]
-[assembly:JsType(JsMode.Prototype, Export=false)]
-[assembly: JsNamespace(Namespace="SharpKit.NodeJs", JsNamespace="")]
+[assembly: JsType(JsMode.Prototype, Export = false)]
+[assembly: JsNamespace(Namespace = "SharpKit.NodeJs", JsNamespace = "")]
