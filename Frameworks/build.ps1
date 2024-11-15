fb8bf9b5a74773f2a877c11d7808a4ab4195ee0f
Arakis <sebastian.loncar@gmail.com>
2024-11-15 02:27:21 +0100

    fix-build

--- Frameworks/build.ps1
+++ Frameworks/build.ps1
@@ -5,6 +5,6 @@ Set-Location -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 if($IsLinux) {
     Write-Host "Invoking skc5 on Linux currently not supported" -ForegroundColor Yello
 } else {
-    & C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe\MSBuild.exe Frameworks.sln -verbosity:minimal
+    & "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" Frameworks.sln -verbosity:minimal
 }
 
